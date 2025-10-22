87b8cc4d24a1840e86def10e1bd48184135d9405
Shaan7223 <shanraaaj@gmail.com>
2025-10-22 23:28:40 +0530

    fix: added CORS for Vercel frontend

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import path from 'path';
 import express from 'express';
 import cookieParser from 'cookie-parser';
-
+import cors from 'cors';
 
 // utils
 import connectDB from './config/db.js';
@@ -21,11 +21,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
-
 app.use('/api/users/', userRoutes);
 app.use('/api/category', categoryRoutes);
 app.use('/api/products', productRoutes);
+
+app.use(cors({
+  origin: ['https://safe-store-frontend.vercel.app'], // your frontend URL
+  credentials: true
+}));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
