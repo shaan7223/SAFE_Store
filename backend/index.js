fef2f18a588cc2e25ae4ddb9b83fe6266ff04a47
Shaan7223 <shanraaaj@gmail.com>
2025-10-23 09:15:27 +0530

    fix: changed the endpoints to render url

--- backend/index.js
+++ backend/index.js
@@ -11,11 +11,18 @@ import userRoutes from './routes/userRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 
-dotenv.config({ path: path.resolve('../.env') });
+dotenv.config();
 
 connectDB();
 
 const app = express()
+
+app.use(
+  cors({
+    origin: ["https://safe-store-frontend.vercel.app", "http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,11 +31,6 @@ app.use(cookieParser());
 app.use('/api/users/', userRoutes);
 app.use('/api/category', categoryRoutes);
 app.use('/api/products', productRoutes);
-
-app.use(cors({
-  origin: ['https://safe-store-frontend.vercel.app'], // your frontend URL
-  credentials: true
-}));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = import.meta.env.VITE_API_URL;
-export const USERS_URL = '/api/users'
-export const CATEGORY_url = '/api/category'
+export const BASE_URL = import.meta.env.VITE_API_URL; // e.g. "https://safe-store-backend.onrender.com"
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const CATEGORY_URL = `${BASE_URL}/api/category`;
